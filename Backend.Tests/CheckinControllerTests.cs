5cbcc73b3a1c0148d0baea318f0439313cc341f5
Richard Joy <richardjoy530@gmail.com>
2024-02-17 22:50:16 +0530

    Added tests

---
+++ Backend.Tests/CheckinControllerTests.cs
@@ -0,0 +1,58 @@
+using Backend.Contracts;
+using Backend.Controllers;
+using Backend.Storage;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Backend.Tests
+{
+    public class CheckinControllerTests
+    {
+        private Mock<IFileContainer>? _fileContainer;
+        private Mock<IFileDistributorManager>? _fileDistributorManager;
+
+        [TestCase((string[])[], (string[])[], (string[])[], 0)]
+        [TestCase((string[])["cat"], (string[])[], (string[])["cat"], 1)]
+        [TestCase((string[])["cat"], (string[])[], (string[])["cat"], 1)]
+        [TestCase((string[])[], (string[])[], (string[])["cat", "dog"], 2)]
+        [TestCase((string[])["cat"], (string[])["cat"], (string[])["cat"], 0)]
+        [TestCase((string[])["cat", "dog"], (string[])["cat"], (string[])["cat", "dog"], 1)]
+        public void CheckIn(string[] containerFiles , string[] remoteFiles, string[] totalFiles, int retrivalCount)
+        {
+            _fileContainer = new Mock<IFileContainer>();
+            _fileContainer.Setup(i => i.GetTempFileNames()).Returns(containerFiles);
+            _fileContainer.Setup(i => i.DiscardFiles(It.IsAny<string[]>()));
+
+            _fileDistributorManager = new Mock<IFileDistributorManager>();
+            _fileDistributorManager.Setup(i => i.UpdateFileAvailablity(new HostString("1.1.1.1", 443).ToString(), remoteFiles));
+            _fileDistributorManager.Setup(i => i.GetAllFileNames()).Returns(totalFiles);
+
+            foreach (var fileName in totalFiles)
+            {
+                _fileDistributorManager.Setup(i => i.GetRetrivalLink(fileName)).Returns($"fs1:{fileName}");
+            }
+
+            var controller = GetController();
+            var resp = controller.CheckIn(new AvailableFiles { AvailableFileNames = remoteFiles, HostString = new HostString("1.1.1.1", 443) });
+
+            Assert.That(resp, Is.Not.Null);
+            Assert.That(resp.FileLinks, Is.Not.Null);
+            Assert.That(resp.FileLinks.Count, Is.EqualTo(retrivalCount));
+            foreach (var fileName in resp.FileLinks.Keys)
+            {
+                Assert.That(resp.FileLinks[fileName], Is.EqualTo($"fs1:{fileName}"));
+            }
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        private CheckInController GetController()
+        {
+            return new CheckInController(_fileDistributorManager.Object, _fileContainer.Object);
+        }
+    }
+}
