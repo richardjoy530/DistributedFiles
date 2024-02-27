5d50224fd2a8cc039d881b87767e2efe86ec9efa
Richard Joy <richardjoy530@gmail.com>
2024-02-27 23:16:44 +0530

    introduced discovery server to enable peer to peer over NAT

---
+++ DiscoveryServer/Controllers/DiscoveryController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscoveryServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscoveryController : ControllerBase
+    {
+        private readonly ILogger<DiscoveryController> _logger;
+
+        public DiscoveryController(ILogger<DiscoveryController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public HostString Get()
+        {
+            var ip = HttpContext.Connection.RemoteIpAddress!.MapToIPv4().ToString();
+            var remotehost = new HostString(ip, HttpContext.Connection.RemotePort);
+            _logger.LogInformation("remote host address is: \"{host}\"", remotehost);
+            return remotehost;
+        }
+    }
+}
